e9d8251a336726ceb54a3b4e778b8383a9971d6c
mimorphism <bleach707@gmail.com>
2021-09-28 18:56:15 +0800

    -loading.html - added socket io in script tag -created loading.js file    i)added ajax document ready function that makes a call to the generateFeed route to start the link preview generation process    ii)added socket io event on loading progress

--- views/assets/public/js/loading.js
+++ views/assets/public/js/loading.js
@@ -1,10 +1,20 @@
+var socket = io.connect();
+
+socket.on("connect", () => {
+    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+  });
+
+  socket.on("loading", (percentage) => {
+    console.log(percentage);
+    $('.loading-text').text("GENERATING FEED " + percentage + "%");
+  });
+
 $('document').ready(function(){
     $.ajax({  
         url:'http://localhost:3000/generateFeed',  
         method:'get',  
         dataType:'html',  
         success:function(response){  
-            console.log(response);
             $('.feed').html(response); 
             $('.loader').hide();
               },  
@@ -14,3 +24,6 @@ $('document').ready(function(){
     });  
     
     });
+
+   
+
